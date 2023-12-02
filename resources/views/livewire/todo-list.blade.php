37ce183e17637dcf9857649f1950ac8e500df9b7
Yann Boyongo <yaboyon3@gmail.com>
2023-12-02 08:28:16 +0200

    adding an input and listing todos in the livewire component

--- resources/views/livewire/todo-list.blade.php
+++ resources/views/livewire/todo-list.blade.php
@@ -1,3 +1,11 @@
-<div>
-    {{-- A good traveler has no fixed plans and is not intent upon arriving. --}}
+<div class="flex justify-center">
+    <div>
+        <input type="text" wire:model="task" wire:keydown.enter="addTodo" placeholder="add task">
+
+        @forelse ($todos as $todo)
+            {{ $todo->task }}
+        @empty
+            <p>No task yet!</p>
+        @endforelse
+    </div>
 </div>
